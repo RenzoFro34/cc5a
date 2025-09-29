c73aa29bd8f91a64bfd50854659915e889087e39
RenzoFro34 <renzof@usf.edu>
2025-09-28 21:50:52 -0400

    Add files via upload
    added steps six and seven

--- cc5a.js
+++ cc5a.js
@@ -23,16 +23,16 @@ let employees = [{
  hourlyRate: 45,
  hoursWorked: 40
 }];
-console.log(employees)
 
 function calculateBasePay(rate, hours) {
-if (hoursWorked <= 40)
+if (hours <= 40)
     return (hours * rate)
+else null
 ;}
 employees.forEach(employee => {employee.basePay = calculateBasePay});
 
-function calculateOvertimePay(rate ,hours)
-{if (hoursWorked > 40) 
+function calculateOvertimePay(rate, hours)
+{if (hours > 40) 
     return (hours - 40) * rate * 1.5};
 employees.forEach(employee => {employee.overtimePay = calculateOvertimePay});
 
@@ -41,3 +41,18 @@ let taxRate = 0.15;
 return grossPay * taxRate;
 }
 
+function processPayroll(employee) {
+    let name = employee.name;
+    let basePay = calculateBasePay(employee.hourlyRate, employee.hoursWorked);
+    let overtimePay = calculateOvertimePay(employee.hourlyRate, employee.hoursWorked);
+    let grossPay = basePay + overtimePay;
+    let taxes = calculateTaxes(grossPay);
+    let netPay = grossPay - taxes;
+    return {
+    name, basePay, overtimePay, grossPay, netPay
+};}
+
+ employees.forEach(employee => {
+    let payroll = processPayroll(employee);
+    console.log(payroll);
+ });
